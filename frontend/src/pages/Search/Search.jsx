1e54a8e3412fe414bc5f46b02661fc3acd72b0ec
unknown <mohammed_alrushdy@yahoo.com>
2024-01-23 13:45:57 +0300

    working in the search sidebar

--- frontend/src/pages/Search/Search.jsx
+++ frontend/src/pages/Search/Search.jsx
@@ -1,6 +1,7 @@
 import Button from "../../components/Button/Button";
 import Input from "../../components/Input/Input";
 import InputDate from "../../components/InputDate/InputDate";
+import { data } from "../../data/tags";
 import "./Search.scss";
 
 const Search = () => {
@@ -61,9 +62,71 @@ const Search = () => {
             </div>
           </form>
         </div>
-        <div className="search-sidebar">search-sidebar</div>
+        <div className="search-sidebar">
+          <div className="in_out">
+            <fieldset>
+              <div className="outgoing_radio">
+                <input
+                  type="radio"
+                  id="outgoing"
+                  name="incomingOrOutgoing"
+                  value="صادر"
+                  checked
+                />
+                <label for="outgoing">صادر</label>
+              </div>
+
+              <div>
+                <input
+                  type="radio"
+                  id="incoming"
+                  name="incomingOrOutgoing"
+                  value="وارد"
+                />
+                <label for="incoming">وارد</label>
+              </div>
+            </fieldset>
+          </div>
+          <div className="search-sidebar_tags">
+            <fieldset>
+              {data.map((tag) => {
+                return (
+                  <div key={tag.id}>
+                    <input
+                      type="checkbox"
+                      id={`tag${tag.id}`}
+                      name={tag.tag}
+                      value={tag.id}
+                    />
+                    <label for={`tag${tag.id}`}>{tag.tag}</label>
+                  </div>
+                );
+              })}
+            </fieldset>
+
+            {/* 
+            <fieldset>
+  <legend>Choose your interests</legend>
+  <div>
+    <input type="checkbox" id="coding" name="interest" value="coding" />
+    <label for="coding">Coding</label>
+  </div>
+  <div>
+    <input type="checkbox" id="music" name="interest" value="music" />
+    <label for="music">Music</label>
+  </div>
+</fieldset>
+            */}
+          </div>
+        </div>
       </div>
-      <div className="search-result">search-result</div>
+      <div className="search-result">
+        <p>Serch Result</p>
+        <p>Serch Result</p>
+        <p>Serch Result</p>
+        <p>Serch Result</p>
+        <p>Serch Result</p>
+      </div>
     </div>
   );
 };
