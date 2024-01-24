8f3e0160fb643e49037d38204463d61de83eac6c
Mohammed Alrushdy <mohammed_alrushdy@yahoo.com>
2024-01-24 05:31:26 +0300

    handle changes in the filters

--- frontend/src/pages/Search/Search.jsx
+++ frontend/src/pages/Search/Search.jsx
@@ -1,59 +1,86 @@
-import Button from "../../components/Button/Button";
-import Input from "../../components/Input/Input";
-import InputDate from "../../components/InputDate/InputDate";
-import { data } from "../../data/tags";
-import "./Search.scss";
+import { useState } from 'react';
+
+import Button from '../../components/Button/Button';
+import Input from '../../components/Input/Input';
+import InputDate from '../../components/InputDate/InputDate';
+import { data } from '../../data/tags';
+import { data as ingoingData } from '../../data/ingoing';
+import { data as outgoingData } from '../../data/outgoing';
+import './Search.scss';
 
 const Search = () => {
+  const [filterFields, setFilterFields] = useState({
+    ingoingOrOutgoing: 'صادر',
+    tags: [],
+  });
+
+  const handleFilterFiledsChange = (event) => {
+    if (event.target.name === 'tags') {
+      setFilterFields((prev) => ({
+        ...prev,
+        tags: prev.tags.includes(event.target.value)
+          ? prev.tags.filter((t) => t !== event.target.value)
+          : [...prev.tags, event.target.value],
+      }));
+    } else {
+      setFilterFields((prev) => ({
+        ...prev,
+        [event.target.name]: event.target.value,
+      }));
+    }
+  };
+
+  console.log(filterFields);
+
   return (
-    <div className="search-container">
-      <div className="search-nav">
-        <div className="search-header">
+    <div className='search-container'>
+      <div className='search-nav'>
+        <div className='search-header'>
           <form>
-            <div className="input-fields">
+            <div className='input-fields'>
               <Input
-                type="text"
-                label="الموضوع"
-                id="subject"
+                type='text'
+                label='الموضوع'
+                id='subject'
                 handleChange={(e) => console.log(e.target.value)}
-                value=""
+                value=''
               />
               <Input
-                type="text"
-                label="الجهة"
-                id="correspondent"
+                type='text'
+                label='الجهة'
+                id='correspondent'
                 handleChange={(e) => console.log(e.target.value)}
-                value=""
+                value=''
               />
               <Input
-                type="text"
-                label="صاحب العلاقة"
-                id="employee"
+                type='text'
+                label='صاحب العلاقة'
+                id='employee'
                 handleChange={(e) => console.log(e.target.value)}
-                value=""
+                value=''
               />
             </div>
-            <div className="input-fields">
-              <div className="date-inputs">
+            <div className='input-fields'>
+              <div className='date-inputs'>
                 <span>للفترة</span>
                 <InputDate
-                  id="from"
-                  label="من"
+                  id='from'
+                  label='من'
                   handleChange={() => null}
-                  value=""
+                  value=''
                 />
                 <InputDate
-                  id="to"
-                  label="إلى"
+                  id='to'
+                  label='إلى'
                   handleChange={() => null}
-                  value=""
+                  value=''
                 />
               </div>
 
-              <div className="search-btns">
+              <div className='search-btns'>
                 <Button
-                  className="button primary"
-                  type="submit"
+                  className='button primary'
+                  type='submit'
                   onClick={() => {}}
                 >
                   بحث
@@ -62,65 +89,57 @@ const Search = () => {
             </div>
           </form>
         </div>
-        <div className="search-sidebar">
-          <div className="in_out">
-            <fieldset>
-              <div className="outgoing_radio">
-                <input
-                  type="radio"
-                  id="outgoing"
-                  name="incomingOrOutgoing"
-                  value="صادر"
-                  checked
-                />
-                <label for="outgoing">صادر</label>
-              </div>
+        <div className='search-sidebar'>
+          <div className='search-filter-fields'>
+            <div className='in_out'>
+              <fieldset>
+                <div className='outgoing_radio'>
+                  <input
+                    type='radio'
+                    id='outgoing'
+                    name='ingoingOrOutgoing'
+                    value='صادر'
+                    onChange={handleFilterFiledsChange}
+                    checked={filterFields.ingoingOrOutgoing === 'صادر'}
+                  />
+                  <label htmlFor='outgoing'>صادر</label>
+                </div>
 
-              <div>
-                <input
-                  type="radio"
-                  id="incoming"
-                  name="incomingOrOutgoing"
-                  value="وارد"
-                />
-                <label for="incoming">وارد</label>
-              </div>
-            </fieldset>
-          </div>
-          <div className="search-sidebar_tags">
-            <fieldset>
-              {data.map((tag) => {
-                return (
-                  <div key={tag.id}>
-                    <input
-                      type="checkbox"
-                      id={`tag${tag.id}`}
-                      name={tag.tag}
-                      value={tag.id}
-                    />
-                    <label for={`tag${tag.id}`}>{tag.tag}</label>
-                  </div>
-                );
-              })}
-            </fieldset>
-
-            {/* 
-            <fieldset>
-  <legend>Choose your interests</legend>
-  <div>
-    <input type="checkbox" id="coding" name="interest" value="coding" />
-    <label for="coding">Coding</label>
-  </div>
-  <div>
-    <input type="checkbox" id="music" name="interest" value="music" />
-    <label for="music">Music</label>
-  </div>
-</fieldset>
-            */}
+                <div>
+                  <input
+                    type='radio'
+                    id='ingoing'
+                    name='ingoingOrOutgoing'
+                    value='وارد'
+                    onChange={handleFilterFiledsChange}
+                    checked={filterFields.ingoingOrOutgoing === 'وارد'}
+                  />
+                  <label htmlFor='ingoing'>وارد</label>
+                </div>
+              </fieldset>
+            </div>
+            <div className='search-sidebar_tags'>
+              <fieldset>
+                {data.map((tag) => {
+                  return (
+                    <div key={tag.id}>
+                      <input
+                        type='checkbox'
+                        id={`tag${tag.id}`}
+                        name='tags'
+                        value={tag.id}
+                        onChange={handleFilterFiledsChange}
+                      />
+                      <label htmlFor={`tag${tag.id}`}>{tag.tag}</label>
+                    </div>
+                  );
+                })}
+              </fieldset>
+            </div>
           </div>
         </div>
       </div>
-      <div className="search-result">
+      <div className='search-result'>
         <p>Serch Result</p>
         <p>Serch Result</p>
         <p>Serch Result</p>
